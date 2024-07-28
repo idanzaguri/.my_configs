d03bf4f210015367ef0c700bee79d29dead16bae
idanzaguri <idan.zaguri@gmail.com>
2024-07-28 23:30:04 +0300

    updates

--- apply.sh
+++ apply.sh
@@ -2,6 +2,7 @@ ln -s -r -f .vimrc ~/
 ln -s -r -f .tmux/.tmux.conf ~/
 ln -s -r -f .tmux ~/
 ln -s -r -f .tmux.conf.local ~/
+ln -s -r -f .gtkwaverc ~/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
